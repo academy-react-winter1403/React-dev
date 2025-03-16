cfa9d35cb4dd89d31e62db74ff57f9e3aae06955
MDMasih <mohammadmasih43@gmail.com>
2025-03-17 00:39:26 +0330

    lasted updated

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,7 @@
-import LastLandingWrapper from "./landing/LastLandingWrapper";
-import LastAuthorizeWrapper from "./authorize/LastAuthorizeWrapper";
-import LastCoursesWrapper from "./courses/LastCoursesWrapper";
+// import LastLandingWrapper from "./landing/index";
+import LastLandingWrapper from "./landing";
+// import LastAuthorizeWrapper from "./authorize";
+import LastAuthorizeWrapper from "./authorize";
+import LastCoursesWrapper from "./courses";
 
 export { LastLandingWrapper, LastAuthorizeWrapper, LastCoursesWrapper };
