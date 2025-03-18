c498d6d02b455a9e77f0d00a990a7414731f1120
MDMasih <mohammadmasih43@gmail.com>
2025-03-18 13:31:47 +0330

    create branch feature coursesPage

---
+++ src/core/constants/courses-datas/filter-data.js
@@ -0,0 +1,21 @@
+export const technologyData = [
+    "Boot Strap",
+    "React",
+    "java script",
+    ".Net Core",
+    "tailwind",
+]
+
+export const statusData = [
+    "شروع نشده",
+    "در حال برگزاری",
+    "تمام شده",
+]
+
+export const scoreData = [1, 2, 3, 4, 5]
+
+export const levelData = [
+    "مقدماتی",
+    "متوسط",
+    "پیشرفته",
+]
