ea1b958a7f6ad6dcfffb5cdc17c6e94d3809adc0
MDMasih <mohammadmasih43@gmail.com>
2025-03-18 13:11:12 +0330

    create coueswsPage branch

--- src/components/pages/courses/BottomSection.jsx
+++ src/components/pages/courses/BottomSection.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import FilterLabel from './FilterLabel'
+import React from "react";
+import FilterLabel from "./FilterLabel";
 
 const BottomSection = () => {
   return (
-    <div className='bottom-section-container w-full mt-16'>
-      <div className="main w-[50%]">
+    <div className="bottom-section-container w-full mt-16 flex justify-center gap-x-[10px] items-start">
+      <div className="side p-2 bg-[#FFFFFF] drop-shadow-[0_1px_2px_#0000004D] rounded-[10px]">
+        <FilterLabel />
+      </div>
+      <div className="main w-[70%] h-[200px] border border-red-400">
         <div className="sort-viw-btn-control"></div>
         <div className="product-card-container"></div>
       </div>
-      <div className="side p-2 bg-[#FFFFFF] drop-shadow-[0_1px_2px_#0000004D]">
-        <FilterLabel/>
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BottomSection
+export default BottomSection;
