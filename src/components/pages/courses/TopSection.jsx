540af54c53c277a5053ed366e43ea8ccdbb6c5e7
MDMasih <mohammadmasih43@gmail.com>
2025-03-22 18:23:24 +0330

    updated coursesPage

--- src/components/pages/courses/TopSection.jsx
+++ src/components/pages/courses/TopSection.jsx
@@ -1,47 +1,51 @@
 import React from "react";
 import { CoursesCharectorIcon } from "../../../core/icons/icons";
 import Input from "../../common/input/Input";
+import { motion } from "framer-motion";
+import { MotionComp } from "../../partials";
 
 const TopSection = () => {
   return (
-    <div 
-      className="top-section-contaienr relative w-[80%] max-md:flex max-md:flex-col-reverse max-lg:items-center"
-    >
-      <div
-        className="top-section-control bg-[#EBF9F982] backdrop-blur-[4px]
-        drop-shadow-[0_2px_2px_#00000040] rounded-[15px] p-[40px]
-        max-md:w-full max-md:flex max-md:flex-col max-md:items-center max-md:mt-[20px]"
-      >
-        <div className="text-control">
-          <div className="top flex items-center max-md:flex max-md:flex-col max-md:items-center">
-            <svg
-              width="13"
-              height="14"
-              viewBox="0 0 13 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="relative max-md:mt-[-52px] max-md:mb-[21px] w-[23px] h-[23px]"
-            >
-              <circle cx="6.5" cy="7.11798" r="6.5" fill="#01CEC9" />
-            </svg>
-            <h2
-              className="font-b-yekan text-[25px] font-extrabold text-[#000000]
-              m-0 mr-[10px] max-md:text-center"
-            >
+    <div className="top-section-contaienr relative w-[80%] max-md:flex max-md:flex-col-reverse max-lg:items-center">
+      <MotionComp xInitial={100} xAnimate={0} animDuration={1}>
+        <div
+          className="top-section-control bg-[#EBF9F982] backdrop-blur-[4px]
+            drop-shadow-[0_2px_2px_#00000040] rounded-[15px] p-[40px]
+            max-md:w-full max-md:flex max-md:flex-col max-md:items-center max-md:mt-[20px]"
+        >
+          <div className="text-control">
+            <div className="top flex items-center max-md:flex max-md:flex-col max-md:items-center">
+              <svg
+                width="13"
+                height="14"
+                viewBox="0 0 13 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="relative max-md:mt-[-52px] max-md:mb-[21px] w-[23px] h-[23px]"
+              >
+                <circle cx="6.5" cy="7.11798" r="6.5" fill="#01CEC9" />
+              </svg>
+              <h2
+                className="font-b-yekan text-[25px] font-extrabold text-[#000000]
+                  m-0 mr-[10px] max-md:text-center"
+              >
+                {" "}
+                دوره های آموزشی{" "}
+              </h2>
+            </div>
+            <p className="font-[400] text-[#AAAAAA] indent-[10px] mt-[5px] max-md:text-center">
               {" "}
-              دوره های آموزشی{" "}
-            </h2>
-          </div>
-          <p className="font-[400] text-[#AAAAAA] indent-[10px] mt-[5px] max-md:text-center">
-            {" "}
-            به روز ترین دوره هایی که میتونید پیدا کنید{" "}
-          </p>
-          <div className="input-control w-[60%] mt-[25px] mr-[10px] max-md:w-full">
-            <Input type={"text"} placeholder={"چی میخوای یاد بگیری؟..."} />
+              به روز ترین دوره هایی که میتونید پیدا کنید{" "}
+            </p>
+            <div className="input-control w-[60%] mt-[25px] mr-[10px] max-md:w-full">
+              <Input type={"text"} placeholder={"چی میخوای یاد بگیری؟..."} />
+            </div>
           </div>
         </div>
-      </div>
-      <CoursesCharectorIcon />
+      </MotionComp>
+      <MotionComp xInitial={-100} xAnimate={0} animDuration={1}>
+        <CoursesCharectorIcon />
+      </MotionComp>
     </div>
   );
 };
--- src/components/partials/index.jsx
+++ src/components/partials/index.jsx
@@ -1,5 +1,6 @@
 import CardWrapper from "./product-card/CardWrapper";
 import CardLoading from "./product-card-loading/CardLoading";
 import PaginationData from "./pagination/PaginationData";
+import MotionComp from "./motion/MotionComp";
 
-export { CardWrapper, CardLoading, PaginationData }
+export { CardWrapper, CardLoading, PaginationData, MotionComp }
