842f97d607de33a5c89dec42ff9afd45b42f20e6
Arghavan <arinkr7@gmail.com>
2025-06-12 02:22:21 +0330

    last update of error 404

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+// module.exports = {
+//   darkMode: "class", // یا 'media' برای استفاده از تنظیمات سیستم کاربر
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+
 module.exports = {
-  darkMode: "class", // یا 'media' برای استفاده از تنظیمات سیستم کاربر
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cherry: ['"Cherry Bomb"', 'cursive'], 
+      },
+    },
   },
   variants: {
     extend: {},
